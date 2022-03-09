b305c061966894ad923e1d7ecbd8cdc869cbd90b
Khor <jing.yong.khor@intel.com>
2022-03-09 20:33:53 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const list = ref([1, 2, 3])
+
+const itemRefs = ref([])
+
+onMounted(() => {
+  alert(itemRefs.value.map(i => i.textContent))
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in list" ref="itemRefs">{{ item }}</li>
+  </ul>
+</template>
